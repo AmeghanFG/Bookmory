d34663e0d8ec0cde16d83e897bdca063f5250b81
Imelda Gtz <igutierrez22@ucol.mx>
2026-05-26 19:43:05 -0600

    Se añadió:     Required en campos.   Genero.cs

--- Data/Genero.cs
+++ Data/Genero.cs
@@ -1,8 +1,12 @@
-﻿namespace Bookmory.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookmory.Data
 {
     public class Genero
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Genero_txt { get; set; }
         virtual public List<Libro> Libros { get; set; }
     }
