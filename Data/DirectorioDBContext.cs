e2766b8e79fd75501d439521a6a38189ff15b39b
Meghan <afigueroa57@ucol.mx>
2026-05-27 00:47:32 -0600

    Se agregó la tabla "MisLibros" y datos de estados

--- Data/DirectorioDBContext.cs
+++ Data/DirectorioDBContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Bookmory.Components.Pages;
+using Microsoft.EntityFrameworkCore;
 namespace Bookmory.Data
 {
     public class DirectorioDBContext(DbContextOptions<DirectorioDBContext> options) : DbContext(options)
     {
         public DbSet<Libro> Libros { get; set; }
+        public DbSet<MiLibro> MisLibros { get; set; }
         public DbSet<Autor> Autores { get; set; }
         public DbSet<Estado> Estados { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
---
+++ Migrations/20260527064307_AgregarMisLibros.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookmory.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarMisLibros : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MisLibros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LibroId = table.Column<int>(type: "int", nullable: false),
+                    EstadoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MisLibros", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MisLibros_Estados_EstadoId",
+                        column: x => x.EstadoId,
+                        principalTable: "Estados",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MisLibros_Libros_LibroId",
+                        column: x => x.LibroId,
+                        principalTable: "Libros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MisLibros_EstadoId",
+                table: "MisLibros",
+                column: "EstadoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MisLibros_LibroId",
+                table: "MisLibros",
+                column: "LibroId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MisLibros");
+        }
+    }
+}
