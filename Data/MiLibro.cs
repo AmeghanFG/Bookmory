397e78ddf434003b11682da8c52ba5c58c9c8fad
Meghan <afigueroa57@ucol.mx>
2026-05-27 00:39:30 -0600

    Ya se muestran los libros de la BD en catalogo, creación de clase para libro

---
+++ Data/MiLibro.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookmory.Data
+{
+    public class MiLibro
+    {
+        public int Id { get; set; }
+
+        // Relación con Libro
+        public int LibroId { get; set; }
+        public virtual Libro? Libro { get; set; }
+
+        // Relación con Estado
+        public int EstadoId { get; set; }
+        public virtual Estado? Estado { get; set; }
+    }
+}
