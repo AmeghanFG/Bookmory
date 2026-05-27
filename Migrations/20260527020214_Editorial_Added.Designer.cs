5ab41c0e6666a8a21f4aad2cc3d7c84fdcc15dc8
Imelda Gtz <igutierrez22@ucol.mx>
2026-05-26 20:04:35 -0600

    Se añadió:     Tabla Editorial.   Database     Migración Editorial_Added.  dir Migrations

--- Migrations/20260527020214_Editorial_Added.Designer.cs
+++ Migrations/20260527020214_Editorial_Added.Designer.cs
@@ -4,6 +4,7 @@ using Bookmory.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bookmory.Migrations
 {
     [DbContext(typeof(DirectorioDBContext))]
-    partial class DirectorioDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260527020214_Editorial_Added")]
+    partial class Editorial_Added
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
